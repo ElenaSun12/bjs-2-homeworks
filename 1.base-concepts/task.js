a63e82a72fa4ef622dbf03101198cff5698ca38d
Netology Test <elen.1996@list.ru>
2025-05-23 20:15:48 +0300

    solution to task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+	let arr = [];
+	const discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant < 0) {
+		return arr; // Нет корней
+	}
+
+	if (discriminant === 0) {
+		arr.push(-b / (2 * a)); // Один корень
+		return arr;
+	}
+
+	// Два корня
+	const sqrtD = Math.sqrt(discriminant);
+	arr.push(
+		(-b + sqrtD) / (2 * a),
+		(-b - sqrtD) / (2 * a)
+	);
+
+	return arr;
 }
