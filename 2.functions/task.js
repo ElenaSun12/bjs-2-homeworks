9cc3d3beec106f26705cc43e070abf2227bffb6e
Netology Test <elen.1996@list.ru>
2025-05-27 21:07:27 +0300

    solution to task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,27 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = Infinity;
+  let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max= arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  const avg = +(sum / arr.length).toFixed(2);
+  return { max: max, min: min, avg: avg };
 }
+
+console.log(getArrayParams(-99, 99, 10), '{min: -99, max: 99, avg 3,33}') // { min: -99, max: 99, avg: 3.33 })
+console.log(getArrayParams(1, 2, 3, -100, 10), '{ min: -100, max: 10, avg: -16,80}') // { min: -100, max: 10, avg: -16.80 }
+console.log(getArrayParams(5), 'expect { min: 5, max: 5, avg: 5.00 }')  // { min: 5, max: 5, avg: 5 }
 
 function summElementsWorker(...arr) {
 
